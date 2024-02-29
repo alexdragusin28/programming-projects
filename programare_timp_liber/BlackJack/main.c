6c6021252cc4df618e968b56996efa85f76ce63d
alexdragusin28 <138385489+alexdragusin28@users.noreply.github.com>
2024-03-01 01:47:28 +0200

    Add files via upload

--- programare_timp_liber/BlackJack/main.c
+++ programare_timp_liber/BlackJack/main.c
@@ -1,146 +1,150 @@
 #include <stdio.h>
-#include <conio.h>
-
-char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
-int checkwin();
-void board();
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
 int main()
 {
-    int player = 1, i, choice;
+    int card1_player, card2_player, card1_dealer, card2_dealer, player_cards = 0, dealer_cards = 0, card_aux_player, card_aux_dealer, decision;
+    bool player_busted = false;
+    bool dealer_busted = false;
+    bool ace;
+    bool sw_player = false;
+    bool sw_dealer = false;
 
-    char mark;
-    do
+    srand(time(NULL));
+
+    card1_player = 1 + rand()%11;
+    card2_player = 1 + rand()%11;
+    card1_dealer = 1 + rand()%11;
+    card2_dealer = 1 + rand()%11;
+
+    printf("Let's start the BlackJack GAME!!!\n\n");
+    if((card1_player==card2_player) && (card1_player == 11))
+        player_cards = 12; //because ace is 11 or 1, and in this case, we can't have from the beggining 22
+    else player_cards = card1_player + card2_player;
+    printf("You are the player and you have %d and %d, your total is: %d! \n\n", card1_player, card2_player, player_cards);
+    while(sw_player == false)
     {
-        board();
-        player = (player % 2) ? 1 : 2;
-
-        printf("Player %d, enter a number:  ", player);
-        scanf("%d", &choice);
-
-        mark = (player == 1) ? 'X' : 'O';
-
-        if (choice == 1 && square[1] == '1')
-            square[1] = mark;
-
-        else if (choice == 2 && square[2] == '2')
-            square[2] = mark;
-
-        else if (choice == 3 && square[3] == '3')
-            square[3] = mark;
-
-        else if (choice == 4 && square[4] == '4')
-            square[4] = mark;
-
-        else if (choice == 5 && square[5] == '5')
-            square[5] = mark;
-
-        else if (choice == 6 && square[6] == '6')
-            square[6] = mark;
-
-        else if (choice == 7 && square[7] == '7')
-            square[7] = mark;
-
-        else if (choice == 8 && square[8] == '8')
-            square[8] = mark;
-
-        else if (choice == 9 && square[9] == '9')
-            square[9] = mark;
-
-        else
+        if(player_cards == 21)
         {
-            printf("Invalid move ");
-
-            player--;
-            getch();
+            printf("Your total is %d and it's a BLACKJACK!!!\n", player_cards);
+            sw_player = true;
+            break;
         }
-        i = checkwin();
 
-        player++;
-    }while (i ==  - 1);
+        printf("Do you want to draw another card? Type 1 for YES or 0 for NO! ");
+        scanf("%d", &decision);
+        if(decision==1)
+        {
+            ace = false;
+            card_aux_player = 1 + rand()%11;
+            if(card_aux_player == 11)
+                ace = true;
+            else player_cards += card_aux_player;
+            if ((player_cards < 21) && (ace == true))
+            {
+                if(player_cards < 11)
+                {
+                    player_cards += 11;
+                    card_aux_player = 11;
+                }
+                else {
+                        player_cards += 1;
+                        card_aux_player = 1;
+                }
 
-    board();
+                if(player_cards == 21)
+                    {
+                        printf("You draw a %d, your total is %d! It's a BLACKJACK!\n\n", card_aux_player, player_cards);
+                        sw_player == true;
+                        break;
+                    }
+                else {
+                        printf("You draw a %d, your total is: %d! \n\n", card_aux_player, player_cards);
+                }
+            }
+            else if ((player_cards < 21) && (ace == false))
+                {
+                    printf("You draw a %d, your total is: %d! \n\n", card_aux_player, player_cards);
+                }
+            else if (player_cards > 21){
+                    printf("You draw a %d! Your total is %d! You are over 21, you are ELIMINATED!\n\n",card_aux_player, player_cards);
+                    player_busted = true;
+                    break;
+            }
+            else if (player_cards == 21)
+            {
+                printf("You draw a %d! Your total is %d and it's a BLACKJACK!!!\n\n", card_aux_player, player_cards);
+                sw_player = true;
+            }
+        }
+        else if(decision==0)
+        {
+            sw_player = true;
+            printf("Your total cards is %d!\n\n", player_cards);
+            break;
+        }
+    }
 
-    if (i == 1)
-        printf("==>\aPlayer %d win ", --player);
-    else
-        printf("==>\aGame draw");
+    if((card1_dealer==card2_dealer) && (card1_dealer == 11))
+        dealer_cards = 12;
+    else dealer_cards = card1_dealer + card2_dealer;
+    while(sw_dealer == false)
+    {
+        if(player_busted == true)
+            {
+                printf("The dealer has %d and %d and his total is: %d! The dealer WON because you are BUSTED", card1_dealer, card2_dealer, dealer_cards);
+                sw_dealer == true;
+                break;
+            }
+        else printf("The dealer has %d and %d and his total is: %d!\n", card1_dealer, card2_dealer, dealer_cards);
+        while(dealer_cards < 17)
+        {
+            card_aux_dealer = 1 + rand()%11;
+            ace = false;
+            if(card_aux_dealer == 11)
+                ace = true;
+            if(ace == true)
+            {
+                if(dealer_cards < 11)
+                    {
+                        dealer_cards += 11;
+                        card_aux_dealer = 11;
+                    }
+                else {
+                        dealer_cards += 1;
+                        card_aux_dealer = 1;
+                }
+                printf("The dealer drew a %d and his total is %d! \n", card_aux_dealer, dealer_cards);
 
-    getch();
-
+            }
+            else dealer_cards += card_aux_dealer;
+            printf("The dealer drew a %d and his total is %d! \n", card_aux_dealer, dealer_cards);
+            if(dealer_cards >= 17)
+            {
+                sw_dealer = true;
+                break;
+            }
+        }
+        if(dealer_cards > 21)
+            dealer_busted = true;
+    }
+    if(dealer_busted == true)
+        printf("Dealer is busted and the player WON the game with a total of %d!!!", player_cards);
+    if((dealer_busted != true) && (player_busted != true) )
+    {
+        if((player_cards <= 21) && (dealer_cards <=21))
+        {
+        if(player_cards > dealer_cards)
+            printf("The player win this BLACKJACK game against the dealer!!!");
+        else if(player_cards < dealer_cards)
+            printf("The dealer win this BLACKJACK game against the player!!!");
+        else if(player_cards == dealer_cards)
+            printf("It's a draw!!!");
+        }
+    }
     return 0;
+
 }
 
-/*********************************************
-
-FUNCTION TO RETURN GAME STATUS
-1 FOR GAME IS OVER WITH RESULT
--1 FOR GAME IS IN PROGRESS
-O GAME IS OVER AND NO RESULT
- **********************************************/
-
-int checkwin()
-{
-    if (square[1] == square[2] && square[2] == square[3])
-        return 1;
-
-    else if (square[4] == square[5] && square[5] == square[6])
-        return 1;
-
-    else if (square[7] == square[8] && square[8] == square[9])
-        return 1;
-
-    else if (square[1] == square[4] && square[4] == square[7])
-        return 1;
-
-    else if (square[2] == square[5] && square[5] == square[8])
-        return 1;
-
-    else if (square[3] == square[6] && square[6] == square[9])
-        return 1;
-
-    else if (square[1] == square[5] && square[5] == square[9])
-        return 1;
-
-    else if (square[3] == square[5] && square[5] == square[7])
-        return 1;
-
-    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
-        square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7]
-        != '7' && square[8] != '8' && square[9] != '9')
-
-        return 0;
-    else
-        return  - 1;
-}
-
-
-/*******************************************************************
-FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS MARK
- ********************************************************************/
-
-
-void board()
-{
-    system("cls");
-    printf("\n\n\tTic Tac Toe\n\n");
-
-    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
-
-
-    printf("     |     |     \n");
-    printf("  %c  |  %c  |  %c \n", square[1], square[2], square[3]);
-
-    printf("_____|_____|_____\n");
-    printf("     |     |     \n");
-
-    printf("  %c  |  %c  |  %c \n", square[4], square[5], square[6]);
-
-    printf("_____|_____|_____\n");
-    printf("     |     |     \n");
-
-    printf("  %c  |  %c  |  %c \n", square[7], square[8], square[9]);
-
-    printf("     |     |     \n\n");
-}
